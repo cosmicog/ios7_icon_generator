1496018e26e034aef384d353543571756fc98853
cosmicog <orhangazi4401@gmail.com>
2019-07-07 15:46:47 +0300

    Add custom folder support to organize icons

--- make192ios.sh
+++ make192ios.sh
@@ -1,8 +1,14 @@
 convert $1 -resize 192x192 resized.png
 convert -draw "image Screen 0,0 0,0 'gloss-over-192.png'" resized.png glossed.png
-mkdir -p ./out
-filename=${1#"./in/"}
+filename=""
+if [ "$#" == 1 ]; then
+    mkdir -p ./out
+    filename=${1#"./in/"}
+elif [ "$#" == 2 ]; then
+    mkdir -p ./out_$2
+    filename=${1#"./$2/"}
+fi
 echo " writing $filename in out directory"
-composite -compose copy_opacity ios7-icon-192.png glossed.png ./out/$filename
+composite -compose copy_opacity ios7-icon-192.png glossed.png ./out_$2/$filename
 rm resized.png
 rm glossed.png
