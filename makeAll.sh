622e24d97fa26be9b446b75437faa04e81e9820b
cosmicog <orhangazi4401@gmail.com>
2019-07-16 15:51:35 +0300

    One script to rule them all

--- makeAll.sh
+++ makeAll.sh
@@ -2,6 +2,15 @@ if [ -d "$1" ]; then
   for f in ./$1/*.png; do
     ./make192ios.sh "$f" "$1"
   done
+  for f in ./$1/*.png; do
+    ./make192iosG.sh "$f" "$1"
+  done
+  for f in ./$1/*.png; do
+    ./make192circle.sh "$f" "$1"
+  done
+  for f in ./$1/*.png; do
+    ./make192circleG.sh "$f" "$1"
+  done
 else
   echo "There's no folder named $1"
 fi
