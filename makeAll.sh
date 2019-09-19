07f8c8e4dabcda6b36832b0230aa8bfc225ae1ea
cosmicog <orhangazi4401@gmail.com>
2019-09-19 03:24:28 +0300

    Add samsung squircle icon style to givr it a try

--- makeAll.sh
+++ makeAll.sh
@@ -11,6 +11,12 @@ if [ -d "$1" ]; then
   for f in ./$1/*.png; do
     ./make192circleG.sh "$f" "$1"
   done
+  for f in ./$1/*.png; do
+    ./make192sam.sh "$f" "$1"
+  done
+  for f in ./$1/*.png; do
+    ./make192samG.sh "$f" "$1"
+  done
 else
   echo "There's no folder named $1"
 fi
